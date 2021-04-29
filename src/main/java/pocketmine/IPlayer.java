f862a116f5933e89b881d3a451047be07e452e6c
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-04-29 01:04:36 -0400

    Create IPlayer.java

---
+++ src/main/java/pocketmine/IPlayer.java
@@ -0,0 +1,28 @@
+
+package pocketmine;
+
+import pocketmine.permission.ServerOperator;
+import pocketmine.metadata.Metadatable;
+
+public interface IPlayer extends ServerOperator, Metadatable {
+
+	boolean isOnline();
+
+	String getName();
+
+	boolean isBanned();
+
+	void setBanned(boolean value);
+
+	boolean isWhitelisted();
+
+	void setWhitelisted(boolean value);
+
+	Player getPlayer();
+
+	Long getFirstPlayed();
+
+	Long getLastPlayed();
+
+	boolean hasPlayedBefore();
+}
