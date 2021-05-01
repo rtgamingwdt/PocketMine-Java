77e86b98055d3ff1aa926175931bcb4a56d5c871
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-05-01 01:52:52 -0400

    Update OfflinePlayer.java

--- src/main/java/pocketmine/OfflinePlayer.java
+++ src/main/java/pocketmine/OfflinePlayer.java
@@ -1,5 +1,6 @@
 package pocketmine;
 
+import pocketmine.metadata.Metadatable;
 import pocketmine.metadata.MetadataValue;
 import pocketmine.nbt.tag.CompoundTag;
 import pocketmine.plugin.Plugin;
@@ -7,159 +8,194 @@ import pocketmine.plugin.Plugin;
 import java.util.List;
 import java.util.UUID;
 
-public class OfflinePlayer implements IPlayer {
+public class OfflinePlayer implements IPlayer, Metadatable
+{
   private final Server server;
-  
+
   private final CompoundTag namedTag;
 
-  public OfflinePlayer(Server server, UUID uuid) {
-    this(server, uuid, null);
+  public OfflinePlayer (Server server, UUID uuid)
+  {
+    this (server, uuid, null);
   }
 
-  public OfflinePlayer(Server server, String name) {
-    this(server, null, name);
+  public OfflinePlayer (Server server, String name)
+  {
+    this (server, null, name);
   }
 
-  public OfflinePlayer(Server server, UUID uuid, String name) {
-    this.server = server;
+  public OfflinePlayer (Server server, UUID uuid, String name)
+  {
 
     CompoundTag nbt;
-    
-    if (uuid != null) {
-      nbt = this.server.getOfflinePlayerData(uuid, false);
-    } else if (name != null) {
-      nbt = this.server.getOfflinePlayerData(name, false);
-    } else {
-      throw new IllegalArgumentException("Name and UUID cannot both be null");
-    }
-    
-    if (nbt == null) {
-      nbt = new CompoundTag();
-    }
-    
+
+    if (uuid != null)
+      {
+	nbt = this.server.getOfflinePlayerData (uuid, false);
+      }
+    else if (name != null)
+      {
+	nbt = this.server.getOfflinePlayerData (name, false);
+      }
+    else
+      {
+	throw new
+	  IllegalArgumentException ("Name and UUID cannot both be null");
+      }
+
+    if (nbt == null)
+      {
+	nbt = new CompoundTag ();
+      }
+
     this.namedTag = nbt;
 
-    if (uuid != null) {
-      this.namedTag.putLong("UUIDMost", uuid.getMostSignificantBits());
-      
-      this.namedTag.putLong("UUIDLeast", uuid.getLeastSignificantBits());
-    } else {
-      this.namedTag.putString("NameTag", name);
-    }
-  }
+    if (uuid != null)
+      {
+	this.namedTag.putLong ("UUIDMost", uuid.getMostSignificantBits ());
 
-  @Override
-  public boolean isOnline() {
-    return this.getPlayer() != null;
-  }
-
-  @Override
-  public String getName() {
-    if (namedTag != null && namedTag.contains("NameTag")) {
-      return namedTag.getString("NameTag");
-    }
-    
-    return null;
-  }
-
-  @Override
-  public UUID getUniqueId() {
-    if (namedTag != null) {
-      long least = namedTag.getLong("UUIDLeast");
-      
-      long most = namedTag.getLong("UUIDMost");
-
-      if (least != 0 && most != 0) {
-        return new UUID(most, least);
+	this.namedTag.putLong ("UUIDLeast", uuid.getLeastSignificantBits ());
       }
-    }
-    
+    else
+      {
+	this.namedTag.putString ("NameTag", name);
+      }
+  }
+
+  @Override public boolean isOnline ()
+  {
+    return this.getPlayer () != null;
+  }
+
+  @Override public String getName ()
+  {
+    if (namedTag != null && namedTag.contains ("NameTag"))
+      {
+	return namedTag.getString ("NameTag");
+      }
+
     return null;
   }
 
-  public Server getServer() {
-    return server;
+  @Override public UUID getUniqueId ()
+  {
+    if (namedTag != null)
+      {
+	long least = namedTag.getLong ("UUIDLeast");
+
+	long most = namedTag.getLong ("UUIDMost");
+
+	if (least != 0 && most != 0)
+	  {
+	    return new UUID (most, least);
+	  }
+      }
+
+    return null;
   }
 
-  @Override
-  public boolean isOp() {
-    return this.server.isOp(this.getName().toLowerCase());
+  public Server getServer ()
+  {
+    return this.server;
   }
 
-  @Override
-  public void setOp(boolean value) {
-    if (value == this.isOp()) {
-      return;
-    }
-
-    if (value) {
-      this.server.addOp(this.getName().toLowerCase());
-    } else {
-      this.server.removeOp(this.getName().toLowerCase());
-    }
+  @Override public boolean isOp ()
+  {
+    return this.server.isOp (this.getName ().toLowerCase ());
   }
 
-  @Override
-  public boolean isBanned() {
-    return this.server.getNameBans().isBanned(this.getName());
+  @Override public void setOp (boolean value)
+  {
+    if (value == this.isOp ())
+      {
+	return;
+      }
+
+    if (value)
+      {
+	this.server.addOp (this.getName ().toLowerCase ());
+      }
+    else
+      {
+	this.server.removeOp (this.getName ().toLowerCase ());
+      }
   }
 
-  @Override
-  public void setBanned(boolean value) {
-    if (value) {
-      this.server.getNameBans().addBan(this.getName(), null, null, null);
-    } else {
-      this.server.getNameBans().remove(this.getName());
-    }
+  @Override public boolean isBanned ()
+  {
+    return this.server.getNameBans ().isBanned (this.getName ());
   }
 
-  @Override
-  public boolean isWhitelisted() {
-    return this.server.isWhitelisted(this.getName().toLowerCase());
+  @Override public void setBanned (boolean value)
+  {
+    if (value)
+      {
+	this.server.getNameBans ().addBan (this.getName (), null, null, null);
+      }
+    else
+      {
+	this.server.getNameBans ().remove (this.getName ());
+      }
   }
 
-  @Override
-  public void setWhitelisted(boolean value) {
-    if (value) {
-      this.server.addWhitelist(this.getName().toLowerCase());
-    } else {
-      this.server.removeWhitelist(this.getName().toLowerCase());
-    }
+  @Override public boolean isWhitelisted ()
+  {
+    return this.server.isWhitelisted (this.getName ().toLowerCase ());
   }
 
-  @Override
-  public Player getPlayer() {
-    return this.server.getPlayerExact(this.getName());
+  @Override public void setWhitelisted (boolean value)
+  {
+    if (value)
+      {
+	this.server.addWhitelist (this.getName ().toLowerCase ());
+      }
+    else
+      {
+	this.server.removeWhitelist (this.getName ().toLowerCase ());
+      }
   }
 
-  @Override
-  public Long getFirstPlayed() {
-    return this.namedTag != null ? this.namedTag.getLong("firstPlayed") : null;
+  @Override public Player getPlayer ()
+  {
+    return this.server.getPlayerExact (this.getName ());
   }
 
-  @Override
-  public Long getLastPlayed() {
-    return this.namedTag != null ? this.namedTag.getLong("lastPlayed") : null;
+  @Override public Long getFirstPlayed ()
+  {
+    return this.namedTag !=
+      null ? this.namedTag.getLong ("firstPlayed") : null;
   }
 
-  @Override
-  public boolean hasPlayedBefore() {
+  @Override public Long getLastPlayed ()
+  {
+    return this.namedTag !=
+      null ? this.namedTag.getLong ("lastPlayed") : null;
+  }
+
+  @Override public boolean hasPlayedBefore ()
+  {
     return this.namedTag != null;
   }
 
-  public void setMetadata(String metadataKey, MetadataValue newMetadataValue) {
-    this.server.getPlayerMetadata().setMetadata(this, metadataKey, newMetadataValue);
+  public void setMetadata (String metadataKey, MetadataValue newMetadataValue)
+  {
+    this.server.getPlayerMetadata ().setMetadata (this, metadataKey,
+						  newMetadataValue);
   }
 
-  public List<MetadataValue> getMetadata(String metadataKey) {
-    return this.server.getPlayerMetadata().getMetadata(this, metadataKey);
+  public List < MetadataValue > getMetadata (String metadataKey)
+  {
+    return this.server.getPlayerMetadata ().getMetadata (this, metadataKey);
   }
 
-  public boolean hasMetadata(String metadataKey) {
-    return this.server.getPlayerMetadata().hasMetadata(this, metadataKey);
+  public boolean hasMetadata (String metadataKey)
+  {
+    return this.server.getPlayerMetadata ().hasMetadata (this, metadataKey);
   }
 
-  public void removeMetadata(String metadataKey, Plugin owningPlugin) {
-    this.server.getPlayerMetadata().removeMetadata(this, metadataKey, owningPlugin);
+  public void removeMetadata (String metadataKey, Plugin owningPlugin)
+  {
+    this.server.getPlayerMetadata ().removeMetadata (this, metadataKey,
+						     owningPlugin);
   }
 }
